12916d4b41ce3b1421d34b93aea9bedaf51f7dc6
falconelio <matheusfalconelio@gmail.com>
2025-06-10 20:22:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,31 +20,31 @@ document.addEventListener("keypress", function (event) {
 function makeSound(key) {
 switch (key) {
     case "a":
-        var crash = new Audio("sounds/crash.mp3");
+        var crash = new Audio("Sounds/crash.mp3");
         crash.play();
         break;
     case "s":
-        var tom1 = new Audio("sounds/tom1.mp3");
+        var tom1 = new Audio("Sounds/tom1.mp3");
         tom1.play();
         break;
     case "d":
-        var snare = new Audio("sounds/snare.mp3");
+        var snare = new Audio("Sounds/snare.mp3");
         snare.play();
         break;
     case "g":
-        var kickbass = new Audio("sounds/kickbass.mp3");
+        var kickbass = new Audio("Sounds/kickbass.mp3");
         kickbass.play();
         break;
     case "j":
-        var tom3 = new Audio("sounds/tom3.mp3");
+        var tom3 = new Audio("Sounds/tom3.mp3");
         tom3.play();
         break;
     case "k":
-        var tom2 = new Audio("sounds/tom2.mp3");
+        var tom2 = new Audio("Sounds/tom2.mp3");
         tom2.play();
         break;
     case "l":
-        var crash2 = new Audio("sounds/crash.mp3");
+        var crash2 = new Audio("Sounds/crash.mp3");
         crash2.play();
         break;
     default:
